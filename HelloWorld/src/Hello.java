1f1d1123a46f1d08196ca7f17de00ce14b967e19
Denis <weil@fh-brandenburg.de>
2012-10-24 13:59:07 +0200

    Änderung

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -1,13 +1,13 @@
 
 public class Hello {
 	public static void main(String[]args){
-		System.out.println("Test");
+		System.out.println("Rico");
 		System.out.println("Test");
 		System.out.println("Test");
 
 		System.out.println("Das hier soll ein Merge sein");
 		System.out.println("Hier soll etwas anderes stehen");
-		System.out.println("Das hier soll ein Rico sein");
+		System.out.println("Das hier soll ein Merge sein");
 		
 		for(int i = 0 ; i <= 10 ; i++)
 		{
