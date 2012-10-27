87ee76bacbb51d9241a643627749fdff5a765d9f
Tanja Leschke <Tanja@Tanja-PC>
2012-10-27 11:04:37 +0200

    tagtest

--- tester/src/Testerklasse.java
+++ tester/src/Testerklasse.java
@@ -6,6 +6,8 @@ public class Testerklasse {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Eine Testausgabe");
+		System.out.println("Eine Testausgabe2");
+		System.out.println("Eine Testausgabe3");
 	}
 
 }
